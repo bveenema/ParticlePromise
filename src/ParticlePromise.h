395c36bbd9b7bed78b98b353591a0d9d3d58afce
unknown <bveenema@gmail.com>
2018-05-11 05:27:02 -0400

    Began creating class definition

--- src/ParticlePromise.h
+++ src/ParticlePromise.h
@@ -7,6 +7,7 @@
 #include "Particle.h"
 
 // This is your main class that users will import into their application
+template<size_t bufferSize = 5, size_t maxTopicLenght = 20>
 class ParticlePromise{
 public:
   /**
@@ -17,7 +18,14 @@ public:
   /**
    * Example method
    */
-  void begin();
+  void setTimeout(uint32_t newTimeout){
+    if(newTimeout > 60000) return; // prevent timeouts longer than 60 seconds
+    defaultTimeout = newTimeout;
+  }
+
+  void create(void (*function)(void), const char* responseTopic, uint32_t timeout = defaultTimeout){
+
+  }
 
   /**
    * Example method
@@ -25,8 +33,33 @@ public:
   void process();
 
 private:
+  uint32_t defaultTimeout = 5000; // default timeout time
+
+  struct PromiseContainer{
+    char responseTopic[maxTopicLength];
+    std::function<void(void)> sendWebhookFunc;
+    std::function<void(const char*, const char*)> successFunc;
+    std::function<void(const char*, const char*)> errorFunc;
+    std::function<void(void)> timeoutFunc;
+    std::function<void(void)> finalFunc;
+  };
+
+  PromiseContainer Promises[bufferSize];
+
   /**
    * Example private method
    */
   void doit();
 };
+
+
+ParticlePromise<{bufferSize}, {maxTopicLength}> promise;  // Creates a buffer for function pointers and response topics. And calls Particle.subscribe() with filter "promise-response"
+
+void promise.create(*sendWebhookFunction, "myResponseTopic", *successFunction, {*errorFunction}, {*timeoutFunction}, {int timeout}, {*finalFunction});
+auto index = promise.create(*sendWebhookFunction, "myResonseTopic", {int timeout});
+                index.then(*successFunction);
+                index.catch(*errorFunction);
+                index.timeout(*timeoutFunction, {int timeout});
+                index.finally(*finalFunction);
+
+promise.reEnable(); // Re-creates Particle.subscribe() in case of a previous Particle.unsubscribe() call
