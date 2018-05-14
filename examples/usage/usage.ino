365ec97d6bf70f935abbc84a43a035a0f0d0cb4f
Ben Veenema <bveenema@gmail.com>
2018-05-14 16:00:50 -0400

    Update example

--- examples/usage/usage.ino
+++ examples/usage/usage.ino
@@ -3,14 +3,12 @@
 #include "ParticlePromise.h"
 
 // Initialize objects from the lib
-ParticlePromise particlePromise;
+ParticlePromise Promise();
 
 void setup() {
-    // Call functions on initialized library objects that require hardware
-    particlePromise.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
-    // Use the library's initialized objects and functions
-    particlePromise.process();
+
 }
