a60276b61c182eef74c160a5641868d33f932f71
Ben Veenema <bveenema@gmail.com>
2018-05-14 16:00:59 -0400

    WIP

--- src/ParticlePromise.cpp
+++ src/ParticlePromise.cpp
@@ -1,39 +1,73 @@
-/* ParticlePromise library by Ben Veenema
- */
-
 #include "ParticlePromise.h"
 
-/**
- * Constructor.
- */
-ParticlePromise::ParticlePromise()
-{
-  // be sure not to call anything that requires hardware be initialized here, put those in begin()
+ParticlePromise::ParticlePromise(const uint8_t _containerSize = 5, const uint8_t _maxTopicLength = 20){
+  //
+  Promise<20> * PromiseContainer = * new Promise<20>[5];
+
+  // initialize PromiseContainer
+//   {
+//   using namespace std::placeholders;
+//   // Promise<20> defaultPromise = {
+//   //   false,
+//   //   "null",
+//   //   0,
+//   //   std::bind(defaultFuncA, this, _1, _2),
+//   //   std::bind(defaultFuncA, this, _1, _2),
+//   //   std::bind(defaultFuncB, this),
+//   //   std::bind(defaultFuncB, this)
+//   // };
+//   Promise<20> defaultPromise;
+//   defaultPromise.successFunc = std::bind(defaultFuncA, this, _1, _2);
+// }
+//   for(uint8_t i=0; i<containerSize; i++){
+//     PromiseContainer[i] = defaultPromise;
+//   }
+
+  // Setup Particle.subscribe() handler
+  Particle.subscribe(System.deviceID() + "/promise-response", &ParticlePromise::responseHandler, this, MY_DEVICES);
 }
 
-/**
- * Example method.
- */
-void ParticlePromise::begin()
-{
-    // initialize hardware
-    Serial.println("called begin");
+ParticlePromise& ParticlePromise::create(void (*sendWebhookFunc)(void), const char* responseTopic, uint32_t timeout = 0){
+  // find an empty position in the container
+  uint8_t containerPosition = 0;
+  for(containerPosition; containerPosition<containerSize+1; containerPosition++){
+    if(PromiseContainer[containerPosition].inUse == false) break;
+  }
+  if(containerPosition >= containerSize){
+    lastPromise = -1;
+    return *this;
+  }
+
+  // Update PromiseContainer with new promise
+  PromiseContainer[containerPosition].inUse = true;
+  strcpy(PromiseContainer[containerPosition].responseTopic, responseTopic);
+  {
+    using namespace std::placeholders;
+    PromiseContainer[containerPosition].successFunc = std::bind(defaultFuncA, this, _1, _2);
+    PromiseContainer[containerPosition].errorFunc = std::bind(defaultFuncA, this, _1, _2);
+    PromiseContainer[containerPosition].timeoutFunc = std::bind(defaultFuncB, this);
+    PromiseContainer[containerPosition].finalFunc = std::bind(defaultFuncB, this);
+  }
+
+  // call sendWebhookFunc
+  sendWebhookFunc();
+
+  // calculate the timeout Time
+  if(timeout == 0) timeout = defaultTimeout;
+  PromiseContainer[containerPosition].timeoutTime = millis() + timeout;
+
+  lastPromise = containerPosition;
+  return *this;
 }
 
-/**
- * Example method.
- */
-void ParticlePromise::process()
-{
-    // do something useful
-    Serial.println("called process");
-    doit();
-}
-
-/**
-* Example private method
-*/
-void ParticlePromise::doit()
-{
-    Serial.println("called doit");
+void ParticlePromise::process(){
+  // check for timeouts
+  uint32_t timeNow = millis();
+  for(uint8_t i=0; i<containerSize; i++){
+    if(PromiseContainer[i].timeoutTime >= timeNow){
+      PromiseContainer[i].timeoutFunc();
+      PromiseContainer[i].finalFunc();
+      PromiseContainer[i].inUse = false;
+    }
+  }
 }
--- src/ParticlePromise.h
+++ src/ParticlePromise.h
@@ -1,65 +1,142 @@
 #pragma once
 
+//#include <iostream>
+
+
 /* ParticlePromise library by Ben Veenema
  */
 
-// This will load the definition for common Particle variable types
-#include "Particle.h"
 
-// This is your main class that users will import into their application
-template<size_t bufferSize = 5, size_t maxTopicLenght = 20>
+#include "Particle.h"
+#include <functional>
+
+
 class ParticlePromise{
 public:
   /**
-   * Constructor
+   * Constructor: Intializes PromiseContainer and sets up Particle.subscribe()
    */
-  ParticlePromise();
+  ParticlePromise(const uint8_t, const uint8_t);
 
   /**
-   * Example method
+   * setTimeout: Ovverride the default timeout time (5 seconds). Unit: milliseconds
    */
   void setTimeout(uint32_t newTimeout){
     if(newTimeout > 60000) return; // prevent timeouts longer than 60 seconds
     defaultTimeout = newTimeout;
   }
 
-  void create(void (*function)(void), const char* responseTopic, uint32_t timeout = defaultTimeout){
+  /**
+   * create: check for an open slot in PromiseContainer and create new promise
+   */
+  ParticlePromise& create(void (*sendWebhookFunc)(void), const char* responseTopic, uint32_t timeout = 0);
 
-  }
+
+  // ParticlePromise& then(void (*successFunc)(const char*, const char*)){
+  //   if(lastPromise >= 0){
+  //     PromiseContainer[lastPromise].successFunc = successFunc;
+  //   }
+  //   return *this;
+  // }
+  //
+  // ParticlePromise& then(void (*successFunc)(const char*, const char*), void (*errorFunc)(const char*, const char*)){
+  //   if(lastPromise >= 0){
+  //     PromiseContainer[lastPromise].successFunc = successFunc;
+  //     PromiseContainer[lastPromise].errorFunc = errorFunc;
+  //   }
+  //   return *this;
+  // }
+  //
+  // ParticlePromise& error(void (*errorFunc)(const char*, const char*)){
+  //   if(lastPromise >= 0){
+  //     PromiseContainer[lastPromise].successFunc = errorFunc;
+  //   }
+  //   return *this;
+  // }
+  //
+  // ParticlePromise& timeout(void (*timeoutFunc)(void), uint32_t timeout = 0){
+  //   if(lastPromise >= 0){
+  //     if(timeout != 0){
+  //       PromiseContainer[lastPromise].timeoutTime = millis() + timeout;
+  //     }
+  //     PromiseContainer[lastPromise].timeoutFunc = timeoutFunc;
+  //   }
+  //   return *this;
+  // }
+  //
+  // bool finally(void (*finalFunc)(void)){
+  //   if(lastPromise >= 0){
+  //     PromiseContainer[lastPromise].finalFunc = finalFunc;
+  //     return true;
+  //   }
+  //   return false;
+  // }
 
   /**
-   * Example method
+   * process: checks for expired promises
    */
   void process();
 
 private:
+  uint8_t containerSize = 0;
+  uint8_t maxTopicLength = 0;
   uint32_t defaultTimeout = 5000; // default timeout time
 
-  struct PromiseContainer{
-    char responseTopic[maxTopicLength];
-    std::function<void(void)> sendWebhookFunc;
+  template<uint8_t L>
+  struct Promise{
+    bool inUse;
+    char responseTopic[L];
+    uint32_t timeoutTime;
     std::function<void(const char*, const char*)> successFunc;
     std::function<void(const char*, const char*)> errorFunc;
     std::function<void(void)> timeoutFunc;
     std::function<void(void)> finalFunc;
   };
 
-  PromiseContainer Promises[bufferSize];
+  void* PromiseContainer;
 
-  /**
-   * Example private method
-   */
-  void doit();
+  int8_t lastPromise = -1;
+
+  void responseHandler(const char *event, const char *data) {
+    int8_t promiseID = findPromiseByTopic(event);
+    if(promiseID >= 0){
+      if(strstr(event, "success")){
+        PromiseContainer[promiseID].successFunc(event, data);
+      } else if(strstr(event, "error")){
+        PromiseContainer[promiseID].errorFunc(event, data);
+      }
+      PromiseContainer[promiseID].finalFunc();
+      PromiseContainer[promiseID].inUse = false;
+    }
+  }
+
+  int8_t findPromiseByTopic(const char* event){
+    uint8_t containerPosition = 0;
+    for(containerPosition; containerPosition<containerSize; containerPosition++){
+      if(strstr(event, PromiseContainer[containerPosition].responseTopic)) return containerPosition;
+    }
+    return -1;
+  }
+
+  static void defaultFuncA(const char* doesnt, const char* matter){
+    Serial.printlnf("Function not assigned!");
+    //std::cout << "Hello World!\n";
+  }
+
+  static void defaultFuncB(void){
+    Serial.printlnf("Function not assigned!");
+    //std::cout << "Hello World!\n";
+  }
 };
 
 
-ParticlePromise<{bufferSize}, {maxTopicLength}> promise;  // Creates a buffer for function pointers and response topics. And calls Particle.subscribe() with filter "promise-response"
-
-void promise.create(*sendWebhookFunction, "myResponseTopic", *successFunction, {*errorFunction}, {*timeoutFunction}, {int timeout}, {*finalFunction});
-auto index = promise.create(*sendWebhookFunction, "myResonseTopic", {int timeout});
-                index.then(*successFunction);
-                index.catch(*errorFunction);
-                index.timeout(*timeoutFunction, {int timeout});
-                index.finally(*finalFunction);
-
-promise.reEnable(); // Re-creates Particle.subscribe() in case of a previous Particle.unsubscribe() call
+// ParticlePromise<{bufferSize}, {maxTopicLength}> promise;  // Creates a buffer for function pointers and response topics. And calls Particle.subscribe() with filter "promise-response"
+//
+// void promise.create(*sendWebhookFunction, "myResponseTopic", *successFunction, {*errorFunction}, {*timeoutFunction}, {int timeout}, {*finalFunction});
+// auto index = promise.create(*sendWebhookFunction, "myResonseTopic", {int timeout});
+//                 index.then(*successFunction);
+//                 index.catch(*errorFunction);
+//                 index.timeout(*timeoutFunction, {int timeout});
+//                 index.finally(*finalFunction);
+//
+// promise.reEnable(); // Re-creates Particle.subscribe() in case of a previous Particle.unsubscribe() call
