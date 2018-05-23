33f07debe0182d1fe3c35954d0ed25e5fe40f24b
Ben Veenema <bveenema@gmail.com>
2018-05-23 15:40:40 -0400

    .create() returns P_Promise object. Chain method move to P_Promise obj

--- src/ParticlePromise.cpp
+++ src/ParticlePromise.cpp
@@ -1,12 +1,15 @@
 #include "ParticlePromise.h"
 
 ParticlePromise::ParticlePromise(int _containerSize, int _maxTopicLength){
-  // initialize PromiseContainer
+  // Validate inputs
   containerSize = constrain(_containerSize, 1, 50);
   maxTopicLength = constrain(_maxTopicLength, 4, 63);
-  PromiseContainer.reserve(containerSize);
-  for(int i=0; i<containerSize; i++){
+
+  // Initialize PromiseContainer - Create extra container with value: valid = false;
+  PromiseContainer.reserve(containerSize+1);
+  for(int i=0; i<containerSize+1; i++){
     PromiseContainer[i] = P_Promise(maxTopicLength);
+    PromiseContainer[i].valid = true;
     PromiseContainer[i].inUse = false;
     strcpy(PromiseContainer[i].responseTopic,"null");
     PromiseContainer[i].successFunc = this->defaultFuncA;
@@ -14,22 +17,29 @@ ParticlePromise::ParticlePromise(int _containerSize, int _maxTopicLength){
     PromiseContainer[i].timeoutFunc = this->defaultFuncB;
     PromiseContainer[i].finalFunc = this->defaultFuncB;
   }
-  maxTopicLength = _maxTopicLength;
-  containerSize = _containerSize;
+  PromiseContainer[containerSize].valid = false;
 
   // Setup Particle.subscribe() handler
-  Particle.subscribe(System.deviceID() + "/promise-response", &ParticlePromise::responseHandler, this, MY_DEVICES);
+  this->enable();
 }
 
-ParticlePromise& ParticlePromise::create(void (*sendWebhookFunc)(void), const char* responseTopic, uint32_t timeout){
+/**
+ * setTimeout: Ovverride the default timeout time (5 seconds). Unit: milliseconds
+ */
+void ParticlePromise::setTimeout(uint32_t newTimeout){
+  if(newTimeout > 60000) return; // prevent timeouts longer than 60 seconds
+  defaultTimeout = newTimeout;
+}
+
+P_Promise& ParticlePromise::create(void (*sendWebhookFunc)(void), const char* responseTopic, unsigned int timeout){
     // find an empty position in the container
-  uint8_t containerPosition = 0;
+  unsigned int containerPosition = 0;
   for(containerPosition; containerPosition<containerSize+1; containerPosition++){
     if(PromiseContainer[containerPosition].inUse == false) break;
   }
   if(containerPosition >= containerSize){
-    lastPromise = -1;
-    return *this;
+    // return invalid promise (ie PromiseContainer[].valid == false)
+    return PromiseContainer[containerSize];
   }
 
   // Update PromiseContainer with new promise
@@ -50,53 +60,15 @@ ParticlePromise& ParticlePromise::create(void (*sendWebhookFunc)(void), const ch
   if(timeout == 0) timeout = defaultTimeout;
   PromiseContainer[containerPosition].timeoutTime = millis() + timeout;
 
-  lastPromise = containerPosition;
-  return *this;
+  return PromiseContainer[containerPosition];
 }
 
-ParticlePromise& ParticlePromise::then(void (*successFunc)(const char*, const char*)){
-  if(lastPromise >= 0){
-    PromiseContainer[lastPromise].successFunc = successFunc;
-  }
-  return *this;
+void ParticlePromise::enable(void){
+  Particle.subscribe(System.deviceID() + "/promise-response", &ParticlePromise::responseHandler, this, MY_DEVICES);
 }
-
-ParticlePromise& ParticlePromise::then(void (*successFunc)(const char*, const char*), void (*errorFunc)(const char*, const char*)){
-  if(lastPromise >= 0){
-    PromiseContainer[lastPromise].successFunc = successFunc;
-    PromiseContainer[lastPromise].errorFunc = errorFunc;
-  }
-  return *this;
-}
-
-ParticlePromise& ParticlePromise::error(void (*errorFunc)(const char*, const char*)){
-  if(lastPromise >= 0){
-    PromiseContainer[lastPromise].successFunc = errorFunc;
-  }
-  return *this;
-}
-
-ParticlePromise& ParticlePromise::timeout(void (*timeoutFunc)(void), uint32_t timeout){
-  if(lastPromise >= 0){
-    if(timeout != 0){
-      PromiseContainer[lastPromise].timeoutTime = millis() + timeout;
-    }
-    PromiseContainer[lastPromise].timeoutFunc = timeoutFunc;
-  }
-  return *this;
-}
-
-bool ParticlePromise::finally(void (*finalFunc)(void)){
-  if(lastPromise >= 0){
-    PromiseContainer[lastPromise].finalFunc = finalFunc;
-    return true;
-  }
-  return false;
-}
-
 
 void ParticlePromise::responseHandler(const char *event, const char *data) {
-    int8_t promiseID = findPromiseByTopic(event);
+    int promiseID = findPromiseByTopic(event);
     if(promiseID >= 0){
       if(strstr(event, "success")){
         PromiseContainer[promiseID].successFunc(event, data);
@@ -108,10 +80,10 @@ void ParticlePromise::responseHandler(const char *event, const char *data) {
     }
   }
 
-  int8_t ParticlePromise::findPromiseByTopic(const char* event){
-    uint8_t containerPosition = 0;
-    for(containerPosition; containerPosition<containerSize; containerPosition++){
-      if(strstr(event, PromiseContainer[containerPosition].responseTopic)) return containerPosition;
-    }
-    return -1;
+int ParticlePromise::findPromiseByTopic(const char* event){
+  int containerPosition = 0;
+  for(containerPosition; containerPosition<containerSize; containerPosition++){
+    if(strstr(event, PromiseContainer[containerPosition].responseTopic)) return containerPosition;
   }
+  return -1;
+}
--- src/ParticlePromise.h
+++ src/ParticlePromise.h
@@ -13,13 +13,41 @@
 #include <vector>
 
 class P_Promise{
+  friend class ParticlePromise;
 public:
   P_Promise(unsigned int charLen){
     responseTopic = new char[charLen+1];
   }
+  P_Promise& then(void (*_successFunc)(const char*, const char*)){
+    successFunc = _successFunc;
+    return *this;
+  }
+  P_Promise& then(void (*_successFunc)(const char*, const char*), void (*_errorFunc)(const char*, const char*)){
+    this->then(_successFunc);
+    this->error(_errorFunc);
+    return *this;
+  }
+  P_Promise& error(void (*_errorFunc)(const char*, const char*)){
+    errorFunc = _errorFunc;
+    return *this;
+  }
+  P_Promise& timeout(void (*_timeoutFunc)(void), uint32_t timeout = 0){
+    if(timeout != 0){
+      timeoutTime = millis() + timeout;
+    }
+    timeoutFunc = _timeoutFunc;
+    return *this;
+  }
+  void finally(void (*_finalFunc)(void)){
+    finalFunc = _finalFunc;
+  }
+
+  bool valid;
+
+private:
   bool inUse;
   char* responseTopic;
-  uint32_t timeoutTime;
+  unsigned int timeoutTime;
   std::function<void(const char*, const char*)> successFunc;
   std::function<void(const char*, const char*)> errorFunc;
   std::function<void(void)> timeoutFunc;
@@ -29,22 +57,33 @@ public:
 
 class ParticlePromise{
 public:
+  /**
+   * Constructor: Initializs the PromiseContainer. Optionally set containerSize and maxTopicLength
+   */
   ParticlePromise(int _containerSize = 5, int _maxTopicLength = 20);
 
-  ParticlePromise& create(void (*sendWebhookFunc)(void), const char* responseTopic, uint32_t timeout = 0);
+  /**
+   * setTimeout: Ovverride the default timeout time (5 seconds). Unit: milliseconds
+   */
+  void setTimeout(uint32_t newTimeout);
 
-  ParticlePromise& then(void (*successFunc)(const char*, const char*));
-  ParticlePromise& then(void (*successFunc)(const char*, const char*), void (*errorFunc)(const char*, const char*));
+  /**
+   * enable: enable/re-enable the Particle.subscribe call
+   */
+  void enable(void);
 
-  ParticlePromise& error(void (*errorFunc)(const char*, const char*));
-
-  ParticlePromise& timeout(void (*timeoutFunc)(void), uint32_t timeout = 0);
-
-  bool finally(void (*finalFunc)(void));
+  /**
+   * create: Start a new promise by supplying a function to send the webhook and
+   *          a response topic.  Optionally supply a timeout.  If no timeout is
+   *          supplied, the default will be used.  Returns a reference to the
+   *          promise object
+   */
+  P_Promise& create(void (*sendWebhookFunc)(void), const char* responseTopic, unsigned int timeout = 0);
 
   void printBuffer(){
-    for(int i=0; i<containerSize; i++){
+    for(int i=0; i<containerSize+1; i++){
       Serial.printlnf("PromiseContainer %u", i);
+      Serial.printlnf("\tvalid: %u", PromiseContainer[i].valid);
       Serial.printlnf("\tinUse: %u", PromiseContainer[i].inUse);
       Serial.printlnf("\tresponseTopic: %s", PromiseContainer[i].responseTopic);
       Serial.printlnf("\ttimeoutTime: %u", PromiseContainer[i].timeoutTime);
@@ -56,11 +95,10 @@ private:
   unsigned int containerSize;
   std::vector<P_Promise> PromiseContainer;
   unsigned int defaultTimeout = 5000;
-  unsigned int lastPromise = -1;
 
   void responseHandler(const char *event, const char *data);
 
-  int8_t findPromiseByTopic(const char* event);
+  int findPromiseByTopic(const char* event);
 
   static void defaultFuncA(const char* doesnt, const char* matter){
     Serial.printlnf("Function not assigned!");
