aebc8f79e873c9fec527e3ae62942c5dbc5f53bf
Ben Veenema <bveenema@gmail.com>
2018-05-23 15:46:02 -0400

    Update example

--- examples/usage/usage.ino
+++ examples/usage/usage.ino
@@ -3,7 +3,13 @@
 #include "ParticlePromise.h"
 
 // Initialize objects from the lib
-ParticlePromise promise(5, 60);
+ParticlePromise promise;
+
+void webhookFunc();
+void successFunc(const char*, const char*);
+void errorFunc(const char*, const char*);
+void timeoutFunc();
+void finalFunc();
 
 void setup() {
   Serial.begin(9600);
@@ -11,5 +17,9 @@ void setup() {
 }
 
 void loop() {
+  promise.create(webhookFunc, "Testhook")
+    .then(successFunc);
 
+  auto& p1 = promise.create(webhookFunc, "p3-response");
+  if(p1.valid) p3.then(successFunc).timeout(timeoutFunc);
 }
