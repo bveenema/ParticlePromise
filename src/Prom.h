6e6b07c5c8e848ae447a65751ae6c4f8b475a1eb
Ben Veenema <bveenema@gmail.com>
2018-05-29 15:59:52 -0400

    add class member function storing to .then

--- src/Prom.h
+++ src/Prom.h
@@ -5,10 +5,24 @@ class Prom{
   friend class ParticlePromise;
 public:
   Prom(unsigned int charLen);
+
+  // .then() methods
   Prom& then(void (*_successFunc)(const char*, const char*));
   Prom& then(void (*_successFunc)(const char*, const char*), void (*_errorFunc)(const char*, const char*));
+
+  template <typename T>
+  Prom& then(void (T::*_successFunc)(const char*, const char*), T *instance){
+    using namespace std::placeholders;
+    successFunc = std::bind(_successFunc, instance, _1, _2);
+  }
+
+  // .error() methods
   Prom& error(void (*_errorFunc)(const char*, const char*));
+
+  // .timeout() methods
   Prom& timeout(void (*_timeoutFunc)(void), uint32_t timeout = 0);
+
+  // .finally() methods
   void finally(void (*_finalFunc)(void));
 
   bool valid;
